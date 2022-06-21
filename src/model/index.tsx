fdccc0276f40f788ffe48bde840e420485fe0c5e
paladin9916 <roman.b2055@gmail.com>
2022-06-21 19:15:51 +0200

    feat: theme configuration

---
+++ src/model/index.tsx
@@ -0,0 +1,22 @@
+export type TCard = {
+    id: number,
+    image: {
+        medium: string,
+        original: string
+    },
+    summary: string,
+    name: string
+}
+
+export type TCardResponse = {
+    score: number,
+    show: {
+        id: number,
+        image: {
+            medium: string,
+            original: string
+        },
+        summary: string,
+        name: string
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        base: ['16px'],
+        title: ['20px', '28px'],
+        summary: ['14px', '16px'],
+        btn: ['14px', '18px'],
+        input: ['14px', '18px']
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      colors: {
+        primary: '#202020',
+        base: '#2f2f2f',
+        title: '#101010',
+        summary: '#7f7f7f'
+      }
+    }
   },
   plugins: [],
 }
